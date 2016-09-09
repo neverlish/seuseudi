a2591afe844b8b6f4ac124375c77297ea8cf99c7
blossom14 <wlsdud0581@naver.com>
2016-09-09 14:27:40 +0900

    올라는가는데 텍스트가 표시안됨

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,12 @@
 class CommentsController < ApplicationController
 	def create
-		@project = Project.find(params[:project_id])
-		@comment = @project.comments.create(comment_params)
-		@comment.user = current_user
-		
-		redirect_to project_path(@project)
+		new_comment = Comment.new(body: params[:body],
+								  project_id: params[:project_id],
+								  user_id: current_user.id)
+		new_comment.save
+
+		redirect_to :back
 	end
 
-	private
-		def comment_params
-			params.require(:comment).permit(:body)
-		end
+
 end
