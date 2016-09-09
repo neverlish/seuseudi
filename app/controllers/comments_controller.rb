a423894814c8969ccc927d7a24a7ab7978df6b33
blossom14 <wlsdud0581@naver.com>
2016-09-09 14:38:35 +0900

    댓글삭제까지

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :check_ownership!, only: [:update, :destroy]
 	def create
 		new_comment = Comment.new(body: params[:body],
 								  project_id: params[:project_id],
@@ -8,5 +10,14 @@ class CommentsController < ApplicationController
 		redirect_to :back
 	end
 
+	def destroy
+		@comment.destroy
+		redirect_to :back
+	end
 
+	private
+	def check_ownership!
+		@comment = Comment.find_by(id: params[:id])
+		redirect_to root_path if @comment.user.id != current_user.id
+	end
 end
