97286d9e1623689558e8db4d6458db279b3112ea
Jinho Hyeon <neverlish@gmail.com>
2016-09-09 16:00:17 +0900

    댓글 내용전송 에러 수정

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@ class CommentsController < ApplicationController
 	before_action :authenticate_user!
 	before_action :check_ownership!, only: [:update, :destroy]
 	def create
-		new_comment = Comment.new(body: params[:body],
-								  project_id: params[:project_id],
-								  user_id: current_user.id)
+		new_comment = Comment.new(comment_params)
+		new_comment.user = current_user
+		new_comment.project_id = params[:project_id]
 		new_comment.save
 
 		redirect_to :back
@@ -19,5 +19,9 @@ class CommentsController < ApplicationController
 	def check_ownership!
 		@comment = Comment.find_by(id: params[:id])
 		redirect_to root_path if @comment.user.id != current_user.id
+	end
+
+	def comment_params
+		params.require(:comment).permit(:body)
 	end
 end
